6297c8caf22655e0b620a860a8c5a49b85bd93c7
William Zhang <108840645+WilliamZhang20@users.noreply.github.com>
2025-04-08 07:37:58 -0400

    Delete red obj_dir directory

--- obj_dir/tb.d
+++
@@ -1,5 +0,0 @@
-tb.o: ../tb.cpp Valu.h /usr/share/verilator/include/verilated.h \
- /usr/share/verilator/include/verilatedos.h \
- /usr/share/verilator/include/verilated_config.h \
- /usr/share/verilator/include/verilated_types.h \
- /usr/share/verilator/include/verilated_funcs.h
